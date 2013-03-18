7411e6eb8012523362fb677ef19682b7b1dd9121
Zachary Carter <zack.carter@gmail.com>
2013-03-18 13:11:32 -0700

    fix script paths

--- scripts/setup_couchbase.sh
+++ scripts/setup_couchbase.sh
@@ -2,10 +2,10 @@
 
 sudo service memcached stop
 
-sudo cp ./ci/conf/couchbase.conf /etc/security/limits.d/
+sudo cp ./conf/couchbase.conf /etc/security/limits.d/
 sudo chmod 644 /etc/security/limits.d/couchbase.conf
 wget -q $CB_URL
 sudo dpkg -i $CB_DEB
-sudo chmod a+x ./ci/scripts/cluster_connect
-./ci/scripts/cluster_connect -n 1
+sudo chmod a+x ./scripts/cluster_connect
+./scripts/cluster_connect -n 1
 $CB_CONFIG_CMD

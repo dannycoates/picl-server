7cee42c362a771e3573667979d3ec1bf9792da36
Zachary Carter <zack.carter@gmail.com>
2013-03-18 15:06:29 -0700

    install libcouch using apt-get

--- scripts/setup_couchbase.sh
+++ scripts/setup_couchbase.sh
@@ -4,12 +4,12 @@ sudo service memcached stop
 
 sudo cp ./conf/couchbase.conf /etc/security/limits.d/
 sudo chmod 644 /etc/security/limits.d/couchbase.conf
+sudo wget -O/etc/apt/sources.list.d/couchbase.list http://packages.couchbase.com/ubuntu/couchbase-ubuntu1110.list
+wget -O- http://packages.couchbase.com/ubuntu/couchbase.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libcouchbase2 libcouchbase-dev
 wget -q $CB_URL
 sudo dpkg -i $CB_DEB
-sudo dpkg -i libvbucket1-1_amd64.deb
-sudo dpkg -i libcouchbase1-1_amd64.deb
-sudo dpkg -i libcouchbase-dev-1_amd64.deb
-sudo dpkg -i libcouchbase1-dbg-1_amd64.deb
 sudo chmod a+x ./scripts/cluster_connect
 ./scripts/cluster_connect -n 1
 $CB_CONFIG_CMD
